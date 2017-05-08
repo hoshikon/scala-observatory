1b95aac52457ac7bf2e6352f5894d73bec8e2459
Yasuhide Hoshiko <yasuhide.hoshiko@springer.com>
2017-05-08 14:51:57 +0100

    added a test for Extraction.locateTemperatures()

--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -8,21 +8,30 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class ExtractionTest extends FunSuite {
+  val stationsFile = "/teststations.csv"
+  val temparaturesFile = "/test1986.csv"
+  def toCelsius(f: Double) = (f - 32.0) / 1.8
+
   test("locateTemperatures should read 3 temperatures from one file") {
-    val stationsFile = "/teststations.csv"
-    val temparaturesFile = "/test1986.csv"
     val result = Extraction
       .locateTemperatures(1986, stationsFile, temparaturesFile)
       .toSeq
       .sortBy(_._1.toEpochDay)
 
-    def toCelsius(f: Double) = (f - 32.0) / 1.8
 
     assert(result.size == 3)
     assert(result.head._2 == Location(+70.933,-008.667))
     assert(result(0)._3 == toCelsius(17.2))
     assert(result(1)._3 == toCelsius(12.1))
     assert(result(2)._3 == toCelsius(10.4))
+  }
+
+  test("locationYearlyAverageRecords should return average") {
+    val records = Extraction.locateTemperatures(1986, stationsFile, temparaturesFile)
+    val average = (toCelsius(17.2) + toCelsius(12.1) + toCelsius(10.4))/3
+    val result: Iterable[(Location, Double)] = Extraction.locationYearlyAverageRecords(records)
+
+    assert(result.head._2 == average)
   }
   
 }
