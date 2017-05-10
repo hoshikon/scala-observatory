f22f63602fb51864056b1109e44d42697fe4e674
Yasuhide Hoshiko <yasuhide.hoshiko@springer.com>
2017-05-10 18:02:04 +0100

    test tileLocation()

--- src/test/scala/observatory/InteractionTest.scala
+++ src/test/scala/observatory/InteractionTest.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import java.lang.Math._
+
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -9,5 +11,7 @@ import scala.collection.concurrent.TrieMap
 
 @RunWith(classOf[JUnitRunner])
 class InteractionTest extends FunSuite with Checkers {
-
+  test("tileLocation") {
+    assert(Interaction.tileLocation(0, 0, 0) == Location(atan(sinh(PI)) * 180 / PI, -180.0))
+  }
 }
