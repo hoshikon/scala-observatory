f1e883ff3f5e1d8f0674d5bb090ad53812ce954a
Yasuhide Hoshiko <yasuhide.hoshiko@springer.com>
2017-05-10 18:02:33 +0100

    implement generateTiles()

--- src/main/scala/observatory/Interaction.scala
+++ src/main/scala/observatory/Interaction.scala
@@ -54,7 +54,12 @@ object Interaction {
     yearlyData: Iterable[(Int, Data)],
     generateImage: (Int, Int, Int, Int, Data) => Unit
   ): Unit = {
-    ???
+    for {
+      (year, data) <- yearlyData
+      zoom <- 0 to 3
+      x <- 0 until pow(2, zoom).toInt
+      y <- 0 until pow(2, zoom).toInt
+    } generateImage(year, zoom, x, y, data)
   }
 
 }
