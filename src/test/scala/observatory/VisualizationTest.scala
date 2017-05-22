6b0299d5fbec2d16b0e40b5bd0ffb15d952fe9fd
Yasuhide Hoshiko <yasuhide.hoshiko@springer.com>
2017-05-22 12:28:15 +0100

    Test Visualization class. I know I should have done this first but couldn't help.

--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -1,6 +1,7 @@
 package observatory
 
 
+import observatory.Visualization.{interpolateColor, predictTemperature}
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -9,5 +10,27 @@ import org.scalatest.prop.Checkers
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
 
+  test("predictTemperature should guess temperature from give dataset") {
+    val dataset: Iterable[(Location, Double)] =
+      Seq(
+        (Location(0.0, 0.0), 0.0),
+        (Location(1.0, 0.0), 0.0),
+        (Location(0.0, 1.0), 10.0),
+        (Location(1.0, 1.0), 10.0)
+      )
+    val result = predictTemperature(dataset, Location(0.5, 0.5))
+    assert(result == 5.0)
+  }
+
+  test("interpolateColor should return correct color") {
+    val colors: Iterable[(Double, Color)] =
+      Seq(
+        (0.0, Color(0, 0, 0)),
+        (5.0, Color(50, 0, 0)),
+        (10.0, Color(150, 0, 0))
+      )
+    val result = interpolateColor(colors, 7.5)
+    assert(result.red == 100)
+  }
 
 }
