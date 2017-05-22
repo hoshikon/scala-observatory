955f0ed33cea9098173d9f19a7e86571b42112d7
Yasuhide Hoshiko <yasuhide.hoshiko@springer.com>
2017-05-22 15:18:35 +0100

    Test Visualization2 class

--- src/test/scala/observatory/Visualization2Test.scala
+++ src/test/scala/observatory/Visualization2Test.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import com.sksamuel.scrimage.Pixel
+import observatory.Visualization2._
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -8,13 +10,24 @@ import org.scalatest.prop.Checkers
 @RunWith(classOf[JUnitRunner])
 class Visualization2Test extends FunSuite with Checkers {
   test("bilinearInterpolation should return the middle value if x and y are 0.5") {
-    val center = Visualization2.bilinearInterpolation(0.5, 0.5, 0.0, 1.0, 0.0, 1.0)
+    val center = bilinearInterpolation(0.5, 0.5, 0.0, 1.0, 0.0, 1.0)
     assert(center == 0.5)
   }
 
   test("bilinearInterpolation should return the 1/4 of the value") {
-    val center = Visualization2.bilinearInterpolation(0.78, 0.25, 0.0, 1.0, 0.0, 1.0)
+    val center = bilinearInterpolation(0.78, 0.25, 0.0, 1.0, 0.0, 1.0)
     assert(center == 0.25)
+  }
+
+  test("visualizeGrid should return blank image") {
+    val colors: Iterable[(Double, Color)] =
+      Seq(
+        (-10.0, Color(0, 0, 0)),
+        (0.0, Color(0, 0, 0)),
+        (10.0, Color(0, 0, 0))
+      )
+    val image = visualizeGrid({ case (x: Int, y: Int) => 0.0}, colors, 0, 0, 0)
+    assert(image.pixels.forall(p => p == Pixel(0, 0, 0, 127)))
   }
 
 }
