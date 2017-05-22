32f970f6e3a8aef39f105e58ca8e558a413cbdbc
Yasuhide Hoshiko <yasuhide.hoshiko@springer.com>
2017-05-22 13:20:58 +0100

    Test Manipulation class

--- src/test/scala/observatory/ManipulationTest.scala
+++ src/test/scala/observatory/ManipulationTest.scala
@@ -1,5 +1,6 @@
 package observatory
 
+import observatory.Manipulation._
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -7,5 +8,17 @@ import org.scalatest.prop.Checkers
 
 @RunWith(classOf[JUnitRunner])
 class ManipulationTest extends FunSuite with Checkers {
+  val temps: Iterable[Iterable[(Location, Double)]] =
+    Seq(
+      Seq((Location(0.0, 0.0), 0.0)),
+      Seq((Location(0.0, 0.0), 6.0)),
+      Seq((Location(0.0, 0.0), 15.0))
+    )
+  test("average") {
+    assert(average(temps)(0, 0) == 7.0)
+  }
 
+  test("deviation"){
+    assert(deviation(temps.last, average(temps))(0, 0) == 8.0)
+  }
 }
