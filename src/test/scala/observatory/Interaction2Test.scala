bbe8974c1ed2c838fc80ab168a2cfd55c3fedf62
Yasuhide Hoshiko <yasuhide.hoshiko@springer.com>
2017-05-30 16:36:48 +0100

    Test Interaction2 class

--- src/test/scala/observatory/Interaction2Test.scala
+++ src/test/scala/observatory/Interaction2Test.scala
@@ -1,5 +1,6 @@
 package observatory
 
+import observatory.Interaction2._
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -7,5 +8,10 @@ import org.scalatest.prop.Checkers
 
 @RunWith(classOf[JUnitRunner])
 class Interaction2Test extends FunSuite with Checkers {
-
+  test("yearBound should return different value when layer is updated") {
+    val signal = Var(availableLayers.head)
+    assert(yearBounds(signal)().head == 1975)
+    signal() = availableLayers.last
+    assert(yearBounds(signal)().head == 1991)
+  }
 }
